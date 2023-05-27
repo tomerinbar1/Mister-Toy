15892c63c4bd324a16bdd7f0857b671c14580e2a
tomerinbar1 <tomerinbar1@gmail.com>
2023-05-27 21:00:59 +0300

    Render setup

--- backend/server.js
+++ backend/server.js
@@ -100,9 +100,23 @@ app.delete('/api/toy/:toyId', (req, res) => {
     })
 })
 
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
+}
+
+const port = process.env.PORT || 3030
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+})
 
 // Listen will always be the last line in our server!
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+// app.listen(3030, () => console.log('Server listening on port 3030!'))
